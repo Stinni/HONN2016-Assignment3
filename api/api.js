710afad2c584ab0749083c74fd44f20ee50f8e7d
Stinni <kristinnf13@ru.is>
2016-10-29 11:41:55 +0000

    added *.log to .gitignore and removed punchapi from routes

--- api/api.js
+++ api/api.js
@@ -5,12 +5,10 @@
 const express = require("express");
 const users = require("./v1/usersapi");
 const companies = require("./v1/companiesapi");
-const punches = require("./v1/punchesapi");
 
 const api = express();
 
 api.use("/api/v1/users", users);
 api.use("/api/v1/companies", companies);
-api.use("/api/v1/my/punches", punches);
 
 module.exports = api;
