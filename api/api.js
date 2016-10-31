a1ffa85e8f1861b3443bc61228613d22b77e791d
Stinni <kristinnf13@ru.is>
2016-10-31 23:08:00 +0000

    saving to pull

--- api/api.js
+++ api/api.js
@@ -3,12 +3,14 @@
 // Email: kristinnf13@ru.is
 
 const express = require("express");
+const accounts = require("./v1/accountsapi");
 const users = require("./v1/usersapi");
-const companies = require("./v1/companiesapi");
+const videos = require("./v1/videosapi");
 
 const api = express();
 
+api.use("/api/v1/accounts", accounts);
 api.use("/api/v1/users", users);
-api.use("/api/v1/companies", companies);
+api.use("/api/v1/companies", videos);
 
 module.exports = api;
